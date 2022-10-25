d7f501fe4586c299c716c5990bcbece2192e1511
Flagz29 <116646965+Flagz29@users.noreply.github.com>
2022-10-25 19:38:48 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const tombol = document.getElementsByClassName(`registerbtn`)[0]
+tombol.addEventListener('click', event => {
+    event.preventDefault()
+    alert("kontol")
+})
+
+console.log("kikiw")
